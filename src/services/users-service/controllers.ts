959ee19c1daf6171b5c797070d3179cfe0f0f389
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-10-14 17:35:00 +0200

    add getById, update, and remove to user api

--- src/services/users-service/controllers.ts
+++ src/services/users-service/controllers.ts
@@ -14,6 +14,19 @@ const get = async (req: Request, res: Response) => {
   }
 };
 
+const getById = async (req: Request, res: Response) => {
+  const userId = parseInt(req.params.id);
+  try {
+    const user = await userRepo.findOneBy({ user_id: userId });
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
 const create = async (req: Request, res: Response) => {
   const { first_name, last_name, email, password, phone, role, city } =
     req.body;
@@ -35,4 +48,41 @@ const create = async (req: Request, res: Response) => {
   }
 };
 
-export default { get, create };
+const update = async (req: Request, res: Response) => {
+  const userId = parseInt(req.params.id);
+  const { first_name, last_name, email, password, phone, role, city } =
+    req.body;
+  try {
+    const user = await userRepo.findOneBy({ user_id: userId });
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+    const updateUser = userRepo.merge(user, {
+      first_name,
+      last_name,
+      email,
+      password,
+      phone,
+      role,
+      city,
+    });
+    res.status(200).send(updateUser);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const remove = async (req: Request, res: Response) => {
+  const userId = parseInt(req.params.id);
+  try {
+    const userToRemove = await userRepo.delete(userId);
+    if (userToRemove.affected === 0) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export default { get, getById, create, update, remove };
--- src/services/users-service/routes.ts
+++ src/services/users-service/routes.ts
@@ -4,9 +4,9 @@ import userController from './controllers.js';
 const router = Router();
 
 router.get('/', userController.get);
-// router.get('/:id');
+router.get('/:id', userController.getById);
 router.post('/', userController.create);
-// router.put('/:id');
-// router.delete('/');
+router.put('/:id', userController.update);
+router.delete('/:id', userController.remove);
 
 export default router;
