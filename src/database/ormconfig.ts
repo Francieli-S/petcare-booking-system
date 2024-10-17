7a1a0f8e8822062d8f80f37615dea0800fa9d970
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-10-17 14:57:44 +0200

    move database connections value to .env

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -3,14 +3,17 @@ import { User } from '../entities/User.js';
 import { Sitter } from '../entities/Sitter.js';
 import { PetHuman } from '../entities/PetHuman.js';
 import { Booking } from '../entities/Booking.js';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgresf',
-  database: 'petcare',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
   entities: [User, Sitter, PetHuman, Booking],
