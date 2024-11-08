854becdca8dba49743bb663d48ec4c7f9b775be4
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-11-08 11:42:05 +0100

    add auth controllers, bcrypt, jwt  - wip

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,7 +2,12 @@ import { Request, Response } from 'express';
 import { User } from '../entities/User.js';
 import { AppDataSource } from '../database/ormconfig.js';
 
-const signup = (req: Request, res: Response) => {};
+const userRepo = AppDataSource.getRepository(User)
+
+const signup = (req: Request, res: Response) => {
+  const {email, pass}
+  
+};
 
 const signin = (req: Request, res: Response) => {};
 
