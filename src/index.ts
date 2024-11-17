a723ae6cada0a2382db12f52e36255879321fcec
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-11-17 23:38:00 +0100

    remove vercel config and start a server

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
 import express from 'express';
 import { AppDataSource } from '../src/database/ormconfig.js';
 import routers from '../src/routes/index.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
 app.use('/api', routers);
 
-let isConnected = false
-
 const connectToDB = async () => {
-  if(!isConnected) {
-    try {
-      await AppDataSource.initialize();
-      isConnected = true
-      console.log('DB (PostgreSQL) connected');
-    } catch (error) {
-      console.log('An error occured in the connection to the DB: ', error);
-    }
+  try {
+    await AppDataSource.initialize();
+    console.log('DB (PostgreSQL) connected');
+  } catch (error) {
+    console.log('An error occured in the connection to the DB: ', error);
   }
 };
 
-export { app, connectToDB };
+const startServer = async () => {
+  try {
+    await connectToDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log('Failed to start the server', error);
+  }
+};
+
+startServer();
