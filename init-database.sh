9872020bfd1ce60f4047d9bd40b2b4e6e36e4be8
FrancieliSauthier <francieli.sauthier@gmail.com>
2025-01-19 22:39:31 +0100

    update docker-compose to build the services using separated files to env variables and add volumes to initiate the db

---
+++ init-database.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Connect to the default database 'postgres' and create the required databases
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
+     -- Create the 'petcare_admin' role with a password from the environment variable
+    CREATE ROLE $DB_USERNAME WITH LOGIN PASSWORD '$DB_PASSWORD';
+    
+    -- Allow the role to create databases
+    ALTER ROLE $DB_USERNAME CREATEDB;
+
+    -- Create the required databases and assign ownership to the role
+    CREATE DATABASE $DB_NAME OWNER $DB_USERNAME;
+    CREATE DATABASE petcare_test OWNER $DB_USERNAME;
+EOSQL
