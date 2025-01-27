df4e591e7dcd12b03d77550aca1906e65553673b
FrancieliSauthier <francieli.sauthier@gmail.com>
2025-01-27 17:05:30 +0100

    update entity, request validation, and routes

--- src/middlerwares/validateRequest.ts
+++ src/middlerwares/validateRequest.ts
@@ -2,8 +2,15 @@ import type { Schema } from 'joi';
 import type { NextFunction, Request, Response } from 'express';
 
 export const validateRequest =
-  (schema: { body?: Schema; query?: Schema }) =>
+  (schema: { body?: Schema; query?: Schema; params?: Schema }) =>
   (req: Request, res: Response, next: NextFunction) => {
+    if (schema.params) {
+      const { error } = schema.params.validate(req.params);
+      if (error) {
+        res.status(400).send({ error: error.details[0].message });
+        return;
+      }
+    }
     if (schema.query) {
       const { error } = schema.query.validate(req.query);
       if (error) {
--- src/routes/booking.routes.ts
+++ src/routes/booking.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import bookingController from '../controllers/booking.controllers.js';
+import { validateRequest } from '../middlerwares/validateRequest.js';
+import bookingValidationSchema from '../validations/booking.validations.js'
+import auth from '../middlerwares/auth.js'
 
 const router = Router();
 
-router.get('/');
-router.get('/:id');
-router.post('/');
-router.put('/:id');
-router.delete('/');
+router.get('/', auth, bookingController.getBookings);
+router.get('/:id', auth, bookingController.getBooking);
+router.post('/', validateRequest(bookingValidationSchema.createBooking), auth, bookingController.createBooking
+);
+router.patch('/:id', validateRequest(bookingValidationSchema.updateBooking), auth, bookingController.updateBooking);
+router.delete('/:id', validateRequest(bookingValidationSchema.deleteBooking), auth, bookingController.deleteBooking);
 
 export default router;
