2a71aeb0c6236dc3df48c9c10b1b277c4524414f
FrancieliSauthier <francieli.sauthier@gmail.com>
2025-02-02 00:16:10 +0100

    refactor to separare get sitter by param and get sitter profile by the token of the logged sitter, and remore password from the response

--- src/services/sitter.services.ts
+++ src/services/sitter.services.ts
@@ -1,6 +1,7 @@
 import { AppDataSource } from '../config/database.js';
 import { Sitter } from '../entities/Sitter.js';
 import { User } from '../entities/User.js';
+import { removeSensitiveData } from '../utils/index.js';
 
 const sitterRepo = AppDataSource.getRepository(Sitter);
 
@@ -21,18 +22,11 @@ export const createSitterProfile = async (user: User, bio: string) => {
   return sitter;
 };
 
-export const getSitterProfile = async (userId: number) => {
-  // this is not working properly
-  // user 7 logged who is sitter 2 pass id 2 retrieves user 7 data, but sitter not found message
-  // user 7 logged who is sitter 2 pass id 1 retrieves sitter 1 who is user 1 too
-
-  // user 1 logged who is sitter 1 pass id 1 retrieves sitter 1 who is user 1 too
-  // user 1 logged who is sitter 1 pass id 2 retrieves user 7 data, but sitter not found message
-
+export const getSitterOwnProfile = async (user: User) => {
   const sitter = await sitterRepo.findOne({
     where: {
       user: {
-        id: userId,
+        id: user.id,
       },
     },
     relations: ['user'],
@@ -40,15 +34,31 @@ export const getSitterProfile = async (userId: number) => {
   if (!sitter) {
     throw new Error('Sitter profile not found');
   }
-  // TODO: remove password from the user
-  return sitter;
+  const result = removeSensitiveData(sitter.user);
+  return { ...sitter, user: result };
+};
+
+export const getOneSitter = async (id: number) => {
+  const sitter = await sitterRepo.findOne({
+    where: { id },
+    relations: ['user'],
+  });
+  if (!sitter) {
+    throw new Error('Sitter profile not found');
+  }
+  const result = removeSensitiveData(sitter.user);
+  return { ...sitter, user: result };
 };
 
 export const getAllSitters = async () => {
   const sitters = await sitterRepo.find({
     relations: ['user'],
   });
-  return sitters;
+  const filteredSitters = sitters.map(({ user, ...sitter }) => ({
+    ...sitter,
+    user: removeSensitiveData(user),
+  }));
+  return filteredSitters;
 };
 
 export const updateSitterProfile = async (
